c5ea24664e94315d68d941c497cbe7be5e057ea9
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 15:55:22 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name 
       t.string :location
       t.string :costume_inventory
-      t.string :number_of_employees
+      t.string :num_of_employees
       t.string :still_in_business
       t.string :opening_time
       t.string :closing_time
