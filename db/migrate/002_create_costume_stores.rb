7a44f4b2ac065cfc64f5c71795b1820136fb2fd4
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 15:32:38 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,23 @@
 # Create your costume_stores migration here
+
+class CostumeStores < ActiveRecord::Migration
+  
+  def change 
+    
+    create_table :costume_stores do |t|
+      
+      t.string :name 
+      t.string :location
+      t.string :number_of_costumes
+      t.string :number_of_employees
+      t.string :in_business
+      t.string :opening_time
+      t.string :closing_time
+      
+      t.timestamps 
+      
+    end 
+  
+  end 
+  
+end 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,23 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration
+  
+  def change 
+    
+    create_table :haunted_houses do |t| 
+      
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      t.string :price
+      t.string :family_friendly
+      t.string :opening_date 
+      t.string :closing_date
+      t.string :long_description 
+      
+      t.timestamps 
+      
+    end 
+  
+  end 
+  
+end 
